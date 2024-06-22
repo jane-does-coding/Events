20e7762cfc4429f0f0212c25ff132ee8801ef336
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2024-06-22 13:08:56 -0600

    vercel fixed?

--- app/components/Landing.tsx
+++ app/components/Landing.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import AnimatedTextCharacter from "./Text/AnimatedTextCharacter";
 import AnimatedTextWord from "./Text/AnimatedTextWord";
 import EventsGrid from "./Main/EventsGrid";
+import { TbH1 } from "react-icons/tb";
 
 const Landing = ({ events }: any) => {
 	return (
@@ -15,7 +16,13 @@ const Landing = ({ events }: any) => {
 				className="text-neutral-300 text-[2rem] jura text-center mx-auto w-fit my-0 random-font"
 			/>
 
-			<EventsGrid events={events} />
+			{events.length < 1 ? (
+				<h1 className="mt-[10vh] mx-auto w-fit random-font text-white text-[2rem]">
+					No events found
+				</h1>
+			) : (
+				<EventsGrid events={events} />
+			)}
 		</div>
 	);
 };
